140847f807691e709cb334838c55c646c710378e
ElRafo92 <rafael.martinez@outlook.it>
2022-12-15 17:36:15 +0100

    creato form e funzione password generator

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
+<?php
+    function passwordGenerator() {
+        $alphabet = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789!$%&=?@*";
+        $pass = array(); 
+        $alphaLength = strlen($alphabet) - 1; 
+        for ($i = 0; $i < 8; $i++) {
+            $n = rand(0, $alphaLength);
+            $pass[] = $alphabet[$n];
+        }
+        return implode($pass); 
+    }
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,6 +21,10 @@
     <title>Password Generator</title>
 </head>
 <body>
-    
+    <form action="" method="get">
+        <label for="generator">Inserisci la lunghezza desiderata</label>
+        <input type="number" name="generator" id="" required min="3" max="56" placeholder="min 3 max 56">
+    </form>
+    <h1>Your new password: <?php echo passwordGenerator() ?></h1>
 </body>
 </html>
